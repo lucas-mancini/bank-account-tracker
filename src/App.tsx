e43cb271ddf56bd4d6bff51145ed140b66468ba6
Lucas Mancini <mancini.lucas@gmail.com>
2019-07-08 13:57:52 -0300

    Adding Montserrat webfont.

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,10 @@ const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
+        <h1 className="App-title">Bank account tracker</h1>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   )
