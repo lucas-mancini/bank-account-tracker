76520ba8d9187cff72bafdbbf7b1180f626da626
Lucas Mancini <mancini.lucas@gmail.com>
2019-07-08 14:03:30 -0300

    Adding basic App style.

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,6 @@ const App: React.FC = () => {
     <div className="App">
       <header className="App-header">
         <h1 className="App-title">Bank account tracker</h1>
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
       </header>
     </div>
   )
