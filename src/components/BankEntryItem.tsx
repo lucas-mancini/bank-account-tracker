4ea9de55a563b0162ddda9a6b14b443f73bfd142
Lucas Mancini <mancini.lucas@gmail.com>
2019-07-09 18:51:52 -0300

    Formatting date.

--- src/components/BankEntryItem.tsx
+++ src/components/BankEntryItem.tsx
@@ -13,17 +13,25 @@ export interface BankEntry {
   balances: AccountBalance[]
 }
 
-const BankEntryItem: React.FC<{ bankEntry: BankEntry }> = ({ bankEntry }) => (
-  <div className="BankEntryItem">
-    <div className="BankEntryItem-date">{bankEntry.date.toString()}</div>
-    <ul className="BankEntryItem-balances">
-      {bankEntry.balances.map(balance => (
-        <li
-          key={balance.bankAccountId}
-        >{`${balance.bankAccountId} - ${balance.amount}`}</li>
-      ))}
-    </ul>
-  </div>
-)
+const BankEntryItem: React.FC<{ bankEntry: BankEntry }> = ({ bankEntry }) => {
+  const formattedDate = bankEntry.date.toLocaleString('en-US', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric'
+  })
+
+  return (
+    <div className="BankEntryItem">
+      <div className="BankEntryItem-date">{formattedDate}</div>
+      <ul className="BankEntryItem-balances">
+        {bankEntry.balances.map(balance => (
+          <li
+            key={balance.bankAccountId}
+          >{`${balance.bankAccountId} - ${balance.amount}`}</li>
+        ))}
+      </ul>
+    </div>
+  )
+}
 
 export default BankEntryItem
