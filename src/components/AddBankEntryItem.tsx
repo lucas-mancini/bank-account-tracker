73983ca9a771af9b76a14c1b550f887bfe2526f2
Lucas Mancini <mancini.lucas@gmail.com>
2019-07-15 11:06:48 -0300

    Adding logic for the form that will add the new item.

--- src/components/AddBankEntryItem.tsx
+++ src/components/AddBankEntryItem.tsx
@@ -25,26 +25,41 @@ const accountSelectorOptions = BankAccounts.map(account => ({
   label: account.bankName
 }))
 
+const DEFAULT_NEW_BALANCE = {
+  bankAccountId: accountSelectorOptions[0].value,
+  amount: NumericInput.VALUE_EMPTY
+}
+
 interface AddBankEntryItemProps {
   onAdd: (event: React.FormEvent<HTMLFormElement>) => void
 }
 
 const AddBankEntryItem: React.FC<AddBankEntryItemProps> = ({ onAdd }) => {
   const [selectedDate, setSelectedDate] = useState(new Date())
+  const [balances, setBalances] = useState([DEFAULT_NEW_BALANCE])
+
+  const handleBalanceBankAccountIdChanged = (index: number, bankAccountId: string) => {
+    const updatedBalances = balances.map((balance, balanceIndex) => {
+      return balanceIndex === index ? { ...balance, bankAccountId } : balance
+    })
+    setBalances(updatedBalances)
+  }
+
+  const handleBalanceAmountChanged = (index: number, amount: string) => {
+    const updatedBalances = balances.map((balance, balanceIndex) => {
+      return balanceIndex === index ? { ...balance, amount } : balance
+    })
+    setBalances(updatedBalances)
+  }
+
+  const handleAddNewBalance = () => {
+    setBalances([...balances, DEFAULT_NEW_BALANCE])
+  }
 
   return (
-    <Card
-      interactive={false}
-      elevation={Elevation.FOUR}
-      className="AddBankEntryItem"
-    >
+    <Card interactive={false} elevation={Elevation.FOUR} className="AddBankEntryItem">
       <form onSubmit={onAdd} className="AddBankEntryItem-form">
-        <FormGroup
-          label="Date"
-          labelFor="date-input"
-          labelInfo="(required)"
-          inline={true}
-        >
+        <FormGroup label="Date" labelFor="date-input" labelInfo="(required)" inline={true}>
           <DateInput
             value={selectedDate}
             showActionsBar={true}
@@ -56,30 +71,41 @@ const AddBankEntryItem: React.FC<AddBankEntryItemProps> = ({ onAdd }) => {
             {...jsDateFormatter}
           />
         </FormGroup>
-        <FormGroup label="Account balances" labelFor="balances" inline={false}>
-          <ControlGroup fill={true} id="balances">
-            <HTMLSelect
-              options={accountSelectorOptions}
-              value="Galicia"
-              onChange={(e: React.ChangeEvent<HTMLSelectElement>) => {
-                return console.log(e.currentTarget.value)
-              }}
-            />
-            <NumericInput
-              placeholder="Amount…"
-              leftIcon="dollar"
-              buttonPosition="none"
-              value={1500}
-              onValueChange={(valueAsNumber: number) =>
-                console.log(valueAsNumber)
-              }
-            />
-            <Button
-              className="AddBankEntryItem-new-balance-button"
-              icon="insert"
-              onClick={() => console.log('new balance')}
-            />
-          </ControlGroup>
+        <FormGroup className="AddBankEntryItem-balances" label="Accounts' balances" inline={false}>
+          {balances.map((balance, index) => {
+            return (
+              <ControlGroup
+                className="AddBankEntryItem-balance"
+                fill={false}
+                key={`${balance.bankAccountId}__${index}`}
+              >
+                <HTMLSelect
+                  options={accountSelectorOptions}
+                  value={balance.bankAccountId}
+                  onChange={(e: React.ChangeEvent<HTMLSelectElement>) =>
+                    handleBalanceBankAccountIdChanged(index, e.currentTarget.value)
+                  }
+                />
+                <NumericInput
+                  allowNumericCharactersOnly={true}
+                  placeholder="Amount…"
+                  leftIcon="dollar"
+                  buttonPosition="none"
+                  value={balance.amount}
+                  onValueChange={(_valueAsNumber: number, valueAsString: string) => {
+                    handleBalanceAmountChanged(index, valueAsString)
+                  }}
+                />
+                {index === balances.length - 1 && (
+                  <Button
+                    className="AddBankEntryItem-new-balance-button"
+                    icon="insert"
+                    onClick={handleAddNewBalance}
+                  />
+                )}
+              </ControlGroup>
+            )
+          })}
         </FormGroup>
         <Button
           className="AddBankEntryItem-create-button"
