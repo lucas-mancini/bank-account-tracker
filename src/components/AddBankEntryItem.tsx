69fdb2565d67941f26d6a05ea10b5ab8c9657d1c
Lucas Mancini <mancini.lucas@gmail.com>
2019-07-15 09:37:25 -0300

    Adding basic controls on the forms to include balances.

--- src/components/AddBankEntryItem.tsx
+++ src/components/AddBankEntryItem.tsx
@@ -1,7 +1,17 @@
 import React, { useState } from 'react'
-import { Card, Elevation, Button, Intent, FormGroup } from '@blueprintjs/core'
+import {
+  Card,
+  Elevation,
+  Button,
+  Intent,
+  FormGroup,
+  ControlGroup,
+  HTMLSelect,
+  NumericInput
+} from '@blueprintjs/core'
 import { DateInput, IDateFormatProps } from '@blueprintjs/datetime'
 import { formatDate } from '../util/helpers'
+import BankAccounts from '../config/BankAccounts'
 import './AddBankEntryItem.scss'
 
 const jsDateFormatter: IDateFormatProps = {
@@ -9,6 +19,11 @@ const jsDateFormatter: IDateFormatProps = {
   parseDate: str => new Date(str),
   placeholder: 'MMM DD, YYYY'
 }
+
+const accountSelectorOptions = BankAccounts.map(account => ({
+  value: account.id,
+  label: account.bankName
+}))
 
 interface AddBankEntryItemProps {
   onAdd: (event: React.FormEvent<HTMLFormElement>) => void
@@ -41,8 +56,33 @@ const AddBankEntryItem: React.FC<AddBankEntryItemProps> = ({ onAdd }) => {
             {...jsDateFormatter}
           />
         </FormGroup>
+        <FormGroup label="Account balances" labelFor="balances" inline={false}>
+          <ControlGroup fill={true} id="balances">
+            <HTMLSelect
+              options={accountSelectorOptions}
+              value="Galicia"
+              onChange={(e: React.ChangeEvent<HTMLSelectElement>) => {
+                return console.log(e.currentTarget.value)
+              }}
+            />
+            <NumericInput
+              placeholder="Amount…"
+              leftIcon="dollar"
+              buttonPosition="none"
+              value={1500}
+              onValueChange={(valueAsNumber: number) =>
+                console.log(valueAsNumber)
+              }
+            />
+            <Button
+              className="AddBankEntryItem-new-balance-button"
+              icon="insert"
+              onClick={() => console.log('new balance')}
+            />
+          </ControlGroup>
+        </FormGroup>
         <Button
-          className="AddBankEntryItem-button"
+          className="AddBankEntryItem-create-button"
           intent={Intent.PRIMARY}
           icon="new-object"
           type="submit"
